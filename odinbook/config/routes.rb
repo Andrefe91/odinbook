4862bc7e2a2501e446e8072b83bca6c749e8c3e7
Andres Munoz <andrefe91@gmail.com>
2024-04-25 00:59:27 -0500

    User controller and basic users view

--- odinbook/config/routes.rb
+++ odinbook/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'users/index'
 
   # Defines a resourceful route for posts
   resources :posts
