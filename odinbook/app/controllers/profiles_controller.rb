5b18bba9aa5b19b0c562a0a0c8d3fa52c4b21f40
Andres Munoz <andrefe91@gmail.com>
2024-04-29 12:24:38 -0500

    Username and Name to User Devise Model and Views, updated the seeds generators

--- odinbook/app/controllers/profiles_controller.rb
+++ odinbook/app/controllers/profiles_controller.rb
@@ -1,12 +1,12 @@
 class ProfilesController < ApplicationController
   before_action :set_user
   def show
-
+    @user_posts = @user.posts
   end
 
   private
 
   def set_user
-    @user = User.find_by(email: "#{params[:username]}@gmail.com")
+    @user = User.find_by(username: params[:username])
   end
 end
--- odinbook/db/seeds/1_user.rb
+++ odinbook/db/seeds/1_user.rb
@@ -1,7 +1,10 @@
 #Making sure that this user always gets created
-User.create(email: "andrefe91@gmail.com", password: "password")
+User.create(name: "Andres Felipe Muñoz", username: "andrefe91", email: "andrefe91@gmail.com", password: "password")
 
 #At random, another 19 users will be created
 19.times do
-  User.create(email: "#{Faker::Name.first_name.downcase}@gmail.com", password: "password")
+  name_complete = Faker::Name.name
+  name = name_complete.split(" ")[0].downcase
+
+  User.create(name: name_complete, username: name, email: "#{name}@gmail.com", password: "password")
 end
