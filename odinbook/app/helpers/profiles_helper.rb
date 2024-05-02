71fe1004c4765cc37cbf45385b1ed34d81498556
Andres Munoz <andrefe91@gmail.com>
2024-05-02 15:54:01 -0500

    Gravatar as an option to obtain the User Avatar

--- odinbook/app/helpers/profiles_helper.rb
+++ odinbook/app/helpers/profiles_helper.rb
@@ -1,2 +1,17 @@
 module ProfilesHelper
+  require 'digest/md5'
+
+  #For use with Gravatar, it hashes the email of the user.
+  def get_hash(user)
+    Digest::MD5.hexdigest(user.email.strip.downcase)
+  end
+
+  def gravatar_for(user)
+    params = {
+      :default => "https://storage.googleapis.com/odinbook-test-bucket/profile_default.png",
+      :s => 150
+    }
+
+    url = "https://gravatar.com/avatar/#{get_hash(user)}?#{params.to_query}"
+  end
 end
